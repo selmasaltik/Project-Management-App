a5a4d92a54d56719ed04ac65a6105a06d625a7a3
selmasaltik <selmasaltiik@gmail.com>
2024-09-30 17:48:51 +0300

    Managing State to Switch Between Components

--- 08-project-management-app/src/components/NoProjectSelected.jsx
+++ 08-project-management-app/src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({onStartAddProject}) {
     return (
         <div className="mt-24 text-center w-2/3">
             <img 
@@ -14,7 +14,7 @@ export default function NoProjectSelected() {
             </h2>
             <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
             <p className="mt-8">
-                <Button>
+                <Button onClick={onStartAddProject}>
                     Create new project
                 </Button>
             </p>
